587a7bbe0e3be8e630601a1738e515c4060a8965
Viliam <vatvil@gmail.com>
2023-12-26 15:43:37 +0100

    adding routes

--- app.js
+++ app.js
@@ -3,12 +3,18 @@ const http = require("http");
 const express = require("express");
 
 const app = express();
-app.use((req, res, next) => {
-  console.log("in the middleware");
+
+app.use("/", (req, res, next) => {
+  console.log("This always runs!");
   next();
 });
 
-app.use((req, res, next) => {
+app.use("/product", (req, res, next) => {
+  console.log("in the another middleware");
+  res.send("<h1>The add product page!</h1>");
+});
+
+app.use("/", (req, res, next) => {
   console.log("in the another middleware");
   res.send("<h1>Hello from Express!</h1>");
 });
